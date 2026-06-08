c8df2775e8a27eb6abe96d918983c3adb5a5c173
DakshMaheshwari2830 <dakshmaheshwari2807@gmail.com>
2026-06-08 21:16:46 +0530

    add files

---
+++ using_tuples.py
@@ -0,0 +1,10 @@
+fruits = ("apple", "banana", "orange")
+print("First fruit:", fruits[0])
+print("Second fruit:", fruits[1])
+print("Number of fruits:", len(fruits))
+print("All fruits:")
+for fruit in fruits:
+    print(fruit)
+a, b, c = fruits
+print("Unpacked values:")
+print(a, b, c)
