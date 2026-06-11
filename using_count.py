e7fee77305d89f7d583208a72e78f7618c2e3f12
DakshMaheshwari2830 <dakshmaheshwari2807@gmail.com>
2026-06-11 21:17:06 +0530

    add files

---
+++ using_count.py
@@ -0,0 +1,16 @@
+count=0
+while count<10:
+    print(count)
+    count=count+1
+    if count==3:
+        break
+
+
+
+
+language='Python'
+for letter in language:
+    print(letter)
+    
+for i in range(len(language)):
+    print(language[i])
---
+++ using_operators in dictationaries.py
@@ -0,0 +1,8 @@
+dict={'Daksh','Tanvi','Maheshwari','Ritu'}
+len(dict)
+dict=[('Arun')]
+dict.append('Arun')
+print(dict)
+del dict['Maheshwari']
+print(dict)
+
