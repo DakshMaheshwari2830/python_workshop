9888926789e7dddb019d082b5f3aa24daef0f103
DakshMaheshwari2830 <dakshmaheshwari2807@gmail.com>
2026-06-16 23:49:13 +0530

    update files

--- using_functions.py
+++ using_functions.py
@@ -5,3 +5,21 @@ def sum_of_numbers(n):
     return total
 print(sum_of_numbers(35))
 print(sum_of_numbers(100))
+
+
+def generate_groups(team,*args):
+    print(team)
+    for i in args:
+        print(i)
+generate_groups('Team-1','Daksh','ABC','XYZ','z','k0','hj')
+
+
+def string_reverse(str1):
+    
+    rstr1=''
+    index=len(str1)
+    while index>0:
+        rstr1+=str1[index-1]
+        index=index-1
+    return rstr1
+print(string_reverse("Daksh Maheshwari"))
